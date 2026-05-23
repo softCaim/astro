d903c8b8faa03918c9bb640d4b7985c8ed9c301c
softCaim <vasil.berozashvili.1@iliauni.edu.ge>
2026-05-23 09:50:07 +0400

    davamate 'missing' dictionary, romlitac axal .json failshi anaxlebs mxolod sapovnel cvladebs, im logikit rom cikli edzebs key-ze iset valius romelic aris dictshi da tan misi mnishvneloba aris null, orive roca sruldeba

--- samkutxedi.py
+++ samkutxedi.py
@@ -12,38 +12,55 @@ for i in data:
     C = i.get("C")
     B = i.get("B")
     A = i.get("A")
+    a_c = i.get("a/c")
+    b_c = i.get("b/c")
+
+    missing = {
+        "a": "a" in i and i["a"] is None,
+        "b": "b" in i and i["b"] is None,
+        "c": "c" in i and i["c"] is None,
+        "A": "A" in i and i["A"] is None,
+        "B": "B" in i and i["B"] is None,
+        "C": "C" in i and i["C"] is None,
+        "a/c": "a/c" in i and i["a/c"] is None,
+        "b/c": "b/c" in i and i["b/c"] is None,
+    }
 
     if a and b and c:
         if a + b <= c or a + c <= b or b + c <= a:
             i["error"] = "Not a triangle"
             continue
 
-    if a is not None and b is not None and c is not None and C is None:
+    if a is not None and b is not None and c is not None and missing["C"]:
         Cos_c = (a**2 + b**2 - c**2) / (2 * a * b)
         if Cos_c > 1 or Cos_c < -1:
             i["error"] = "Not a triangle"
             continue
+        else:
+            C = math.degrees(math.acos(Cos_c))
+            i["C"] = C
 
-        i["C"] = math.degrees(math.acos(Cos_c))
-        changed = True
-
-    if a is not None and b is not None and C is not None and c is None:
+    elif a is not None and b is not None and C is not None and missing["c"]:
         c = math.sqrt(a**2 + b**2 - 2 * b * a * math.cos(math.radians(C)))
-        B = math.degrees(math.asin((b * math.sin(math.radians(C))) / c))
-        A = 180 - C - B
         i["c"] = c
-        i["B"] = B
-        i["A"] = A
 
-    if a is not None and b is not None and C is not None and B is None:
+    elif a is not None and b is not None and C is not None and missing["B"]:
+        c = math.sqrt(a**2 + b**2 - 2 * b * a * math.cos(math.radians(C)))
         Sin_B = math.sin(math.radians(C)) * b / c
         if Sin_B > 1 or Sin_B < -1:
             i["error"] = "Not a triangle"
             continue
+        else:
+            B = math.degrees(math.asin(Sin_B))
+            B_1 = 180 - B
+            i["B"] = B
+            if B_1 + C > 180:
+                i["B_1"] = "Not valid"
+                continue
+            else:
+                i["B_1"] = B_1
 
-        i["B"] = math.degrees(math.asin(Sin_B))
-
-    if a is not None and b is not None and A is not None and c is None:
+    elif a is not None and b is not None and A is not None and missing["c"]:
         Sin_B = (b * math.sin(math.radians(A))) / a
         if Sin_B > 1 or Sin_B < -1:
             i["error"] = "Not a triangle"
@@ -51,26 +68,62 @@ for i in data:
 
         B = math.degrees(math.asin(Sin_B))
         C = 180 - A - B
-        c = math.sqrt(a**2 + b**2 - 2 * b * a * math.cos(math.radians(C)))
+
+        if C > 0:
+            c = math.sqrt(a**2 + b**2 - 2 * a * b * math.cos(math.radians(C)))
+            i["c"] = c
+
+        B_1 = 180 - B
+        C_1 = 180 - A - B_1
+
+        if C_1 > 0:
+            c_1 = math.sqrt(a**2 + b**2 - 2 * a * b * math.cos(math.radians(C_1)))
+            i["c_1"] = c_1
+
+    elif a is not None and b is not None and A is not None and missing["B"]:
+        Sin_B = (b * math.sin(math.radians(A))) / a
+        if Sin_B > 1 or Sin_B < -1:
+            i["error"] = "Not a triangle"
+            continue
+        B = math.degrees(math.asin(Sin_B))
         i["B"] = B
-        i["C"] = C
-        i["c"] = c
+        if B + A >= 180:
+            i["error"] = "Not a triangle"
+            continue
+        else:
+            B_1 = 180 - B
+            if B_1 + A >= 180:
+                i["B_1"] = "Not valid"
+                continue
+            else:
+                i["B_1"] = B_1
 
-    if a is not None and b is not None and A is not None and B is None:
-        B = math.degrees(math.asin((a * math.sin(math.radians(A))) / b))
-        i["B"] = B
+    elif a is not None and b is not None and A is not None and missing["C"]:
+        Sin_B = (b * math.sin(math.radians(A))) / a
+        if Sin_B > 1 or Sin_B < -1:
+            i["error"] = "Not a triangle"
+            continue
+        B = math.degrees(math.asin(Sin_B))
+        if B + A >= 180:
+            i["error"] = "Not a triangle"
+            continue
+        else:
+            C = 180 - A - B
+            i["C"] = C
+            B_1 = 180 - B
+            if B_1 + A >= 180:
+                i["B_1"] = "Not valid"
+                continue
+            else:
+                C_1 = 180 - B_1 - A
+                i["C_1"] = C_1
 
-    if a is not None and b is not None and A is not None and C is None:
-        C = 180 - A - B
-        i["C"] = C
-
-    if A is not None and B is not None and a is None and b is None and c is None:
+    elif A is not None and B is not None and missing["a/c"] and missing["b/c"]:
         C = 180 - A - B
 
         x = math.sin(math.radians(A)) / math.sin(math.radians(C))
         y = math.sin(math.radians(B)) / math.sin(math.radians(C))
 
-        i["C"] = C
         i["a/c"] = x
         i["b/c"] = y
         print(x, y)
