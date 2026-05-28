55927d2aa5e04291038102e436118f5b6b409e76
softCaim <vasil.berozashvili.1@iliauni.edu.ge>
2026-05-28 16:54:01 +0400

    sferuli samkutxedis amocanis saboloo versia, yvela pasuxi migebulia da shesabamisobashia.

--- sferuli_samkutxedi.py
+++ sferuli_samkutxedi.py
@@ -1,6 +1,5 @@
 import json
 import math
-from types import NoneType
 
 
 def sin(x):
@@ -65,37 +64,26 @@ for i in data:
         c = arcos((cos(a) * cos(b)) + (sin(a) * sin(b) * cos(C)))
         mnishvneli = sin(c)
         B, B_1 = arcsin(mricxveli / mnishvneli)
-        """if b > a and b > c:
+
+        if b > a and b > c:
             if B > B_1:
                 i["B"] = B
-                print(B)
             else:
                 i["B"] = B_1
-                print(f"B_1: {B_1}, B: {B}")
         else:
-            i["B"] = B
-            i["B_1"] = B_1"""
-
-        if (sin(c) / sin(C)) == (sin(b) / sin(B)) == (sin(b) / sin(B_1)):
             i["B"] = B
             i["B_1"] = B_1
-        else:
-            if (sin(c) / sin(C)) == (sin(b) / sin(B)):
-                i["B"] = B
-            elif (sin(c) / sin(C)) == (sin(b) / sin(B_1)):
-                i["B"] = B_1
-            else:
-                i["B"] = None
-                i["B_1"] = None
 
     elif a is not None and b is not None and A is not None and missing["c"]:
         B, B_1 = arcsin(sin(b) * sin(A) / sin(a))
-        mricxveli = cos(a) * cos(b) - sin(a) * sin(b) * cos(A) * cos(B)
+        mricxveli = (cos(a) * cos(b)) - (sin(a) * sin(b) * cos(A) * cos(B))
         mnishvneli = 1 - (sin(A) * sin(B) * sin(a) * sin(b))
         c = arcos(mricxveli / mnishvneli)
-        mricxveli = cos(a) * cos(b) - sin(a) * sin(b) * cos(A) * cos(B_1)
+        mricxveli = (cos(a) * cos(b)) - (sin(a) * sin(b) * cos(A) * cos(B_1))
         mnishvneli = 1 - (sin(A) * sin(B_1) * sin(a) * sin(b))
         c_1 = arcos(mricxveli / mnishvneli)
+        i["c"] = c
+        i["c_1"] = c_1
 
     elif a is not None and b is not None and A is not None and missing["B"]:
         mricxveli = sin(A) * sin(b)
@@ -132,6 +120,51 @@ for i in data:
     elif c is not None and A is not None and B is not None and missing["C"]:
         C = arcos((sin(A) * sin(B) * cos(c)) - cos(A) * cos(B))
         i["C"] = C
+
+    elif A is not None and B is not None and a is not None and missing["b"]:
+        b, b_1 = arcsin(sin(a) * sin(B) / sin(A))
+        i["b"] = b
+        i["b_1"] = b_1
+
+    elif A is not None and B is not None and a is not None and missing["c"]:
+        b, b_1 = arcsin(sin(a) * sin(B) / sin(A))
+
+        mricxveli = (cos(a) * cos(b) * sin(A) * sin(B)) - (cos(A) * cos(B))
+        mnishvneli = 1 - (sin(b) * sin(a) * sin(B) * sin(A))
+        c = arcos((cos(a) * cos(b)) + (sin(a) * sin(b) * mricxveli / mnishvneli))
+
+        mricxveli_1 = (cos(a) * cos(b_1) * sin(A) * sin(B)) - (cos(A) * cos(B))
+        mnishvneli_1 = 1 - (sin(b_1) * sin(a) * sin(B) * sin(A))
+        c_1 = arcos(
+            (cos(a) * cos(b_1)) + (sin(a) * sin(b_1) * mricxveli_1 / mnishvneli_1)
+        )
+
+        i["c"] = c
+        i["c_1"] = c_1
+
+    elif A is not None and B is not None and a is not None and missing["C"]:
+        b, b_1 = arcsin(sin(a) * sin(B) / sin(A))
+
+        pirveli = (cos(a) * cos(b)) - (sin(a) * sin(b) * cos(A) * cos(B))
+        meore = 1 - (sin(b) * sin(a) * sin(B) * sin(A))
+        mricxveli = (pirveli / meore) - (cos(a) * cos(b))
+        mnishvneli = sin(a) * sin(b)
+        C = arcos(mricxveli / mnishvneli)
+
+        pirveli_1 = (cos(a) * cos(b_1)) - (sin(a) * sin(b_1) * cos(A) * cos(B))
+        meore_1 = 1 - (sin(b_1) * sin(a) * sin(B) * sin(A))
+        mricxveli_1 = (pirveli_1 / meore_1) - (cos(a) * cos(b_1))
+        mnishvneli_1 = sin(a) * sin(b_1)
+        C_1 = arcos(mricxveli_1 / mnishvneli_1)
+
+        i["C"] = C
+        i["C_1"] = C_1
+
+    elif A is not None and B is not None and C is not None and missing["c"]:
+        mricxveli = cos(C) + (cos(A) * cos(B))
+        mnishvneli = sin(A) * sin(B)
+        c = arcos(mricxveli / mnishvneli)
+        i["c"] = c
 
 
 with open("spher_tr.json", "w") as f:
