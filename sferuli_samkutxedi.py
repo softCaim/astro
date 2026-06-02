8c053ff6fc138e3e0dbb8bbb0bd1d47f5abeda17
softCaim <vasil.berozashvili.1@iliauni.edu.ge>
2026-06-02 08:40:56 +0400

    ყველა კუთხის და გვერდის გამოთვლის ფომრულა გატანილია ციკლის გარეთ და არის ფუნქციებში მათი გამოძახების მიზნით ციკლის შიგნით, ასევე missing dict შევცვალე unknown ცვლადით რომელიც იღებს თავდაპირველი json იდან შიდა ციკლის საშუალებით ყველა მნიშვნელობას(ციკლის ცვლადი key უვლის კუთხეების და გვერდების დასახელებას ფუნქცია next ის საშუალებით, მერე იყენებს პირობას, რომ თუ ეს ჩარი არის json ში და თუ მისი მნიშვნელობა არის None), რითიც ვაკეთებ საძებნი მნიშვნელობების ფილტრაციას ყველა ამოცანაში. შემდეგ კი ვიყენებ match case კონსტრუქციას კითხვადობის გაზრდის მიზნით, სადაც თითო ქეისი არის უბრალოდ ყველა ის dict json-ს შიგნით,  სადაც კონკრეტული key-ს მნიშვნელობა არის None(null) და ციკლის საშუალებით ყველა მნიშვნელობებს ვუვლით და ვიძახებთ კონკრეტული კუთხის ან გვერდის საძებნ ფუნქციას, რომელშიც გატანილია if ების საშუალებით ამ საძებნი მნშვნლობის საპოვნი ფორმულები, სხვადასხვა შემთხვევებისთვის

--- sferuli_samkutxedi.py
+++ sferuli_samkutxedi.py
@@ -29,6 +29,147 @@ def arcos(x):
     return None
 
 
+def know(*args):
+    return None not in args
+
+
+def degrees_to_dms(x):
+    if know(x):
+        degrees = int(x)
+        minutes = (x - degrees) * 60
+        return f"{degrees}°{minutes:.1f}'"
+    return None
+
+
+def find_C(i, a, b, c, A, B, C):
+
+    if know(a, b, c):
+        mricxveli = cos(c) - cos(a) * cos(b)
+        mnishvneli = sin(a) * sin(b)
+        C = arcos(mricxveli / mnishvneli)
+        i["C"] = degrees_to_dms(C)
+
+    elif know(a, b, A):
+        B, B_1 = arcsin(sin(b) * sin(A) / sin(a))
+        mricxveli = (sin(A) * sin(B) * cos(a) * cos(b)) - cos(A) * cos(B)
+        mnishvneli = 1 - (sin(A) * sin(B) * sin(a) * sin(b))
+        C = arcos(mricxveli / mnishvneli)
+        mricxveli_1 = (sin(A) * sin(B_1) * cos(a) * cos(b)) - cos(A) * cos(B_1)
+        mnishvneli_1 = 1 - (sin(A) * sin(B_1) * sin(a) * sin(b))
+        C_1 = arcos(mricxveli_1 / mnishvneli_1)
+        i["C"] = degrees_to_dms(C)
+        i["C_1"] = degrees_to_dms(C_1)
+
+    elif know(A, B, c):
+        C = arcos((sin(A) * sin(B) * cos(c)) - cos(A) * cos(B))
+        i["C"] = degrees_to_dms(C)
+
+    elif know(A, B, a):
+        b, b_1 = arcsin(sin(a) * sin(B) / sin(A))
+        pirveli = (cos(a) * cos(b)) - (sin(a) * sin(b) * cos(A) * cos(B))
+        meore = 1 - (sin(b) * sin(a) * sin(B) * sin(A))
+        mricxveli = (pirveli / meore) - (cos(a) * cos(b))
+        mnishvneli = sin(a) * sin(b)
+        C = arcos(mricxveli / mnishvneli)
+
+        pirveli_1 = (cos(a) * cos(b_1)) - (sin(a) * sin(b_1) * cos(A) * cos(B))
+        meore_1 = 1 - (sin(b_1) * sin(a) * sin(B) * sin(A))
+        mricxveli_1 = (pirveli_1 / meore_1) - (cos(a) * cos(b_1))
+        mnishvneli_1 = sin(a) * sin(b_1)
+        C_1 = arcos(mricxveli_1 / mnishvneli_1)
+
+        i["C"] = degrees_to_dms(C)
+        i["C_1"] = degrees_to_dms(C_1)
+
+
+def find_c(i, a, b, c, A, B, C):
+
+    if know(C, b, a):
+        c = arcos(cos(a) * cos(b) + sin(a) * sin(b) * cos(C))
+        i["c"] = degrees_to_dms(c)
+
+    elif know(a, b, A):
+        B, B_1 = arcsin(sin(b) * sin(A) / sin(a))
+
+        mricxveli = (cos(a) * cos(b)) - (sin(a) * sin(b) * cos(A) * cos(B))
+        mnishvneli = 1 - (sin(A) * sin(B) * sin(a) * sin(b))
+        c = arcos(mricxveli / mnishvneli)
+        mricxveli_1 = (cos(a) * cos(b)) - (sin(a) * sin(b) * cos(A) * cos(B_1))
+        mnishvneli_1 = 1 - (sin(A) * sin(B_1) * sin(a) * sin(b))
+        c_1 = arcos(mricxveli_1 / mnishvneli_1)
+        i["c"] = degrees_to_dms(c)
+        i["c_1"] = degrees_to_dms(c_1)
+
+    elif know(a, B, A):
+        b, b_1 = arcsin(sin(a) * sin(B) / sin(A))
+
+        mricxveli = (cos(a) * cos(b) * sin(A) * sin(B)) - (cos(A) * cos(B))
+        mnishvneli = 1 - (sin(b) * sin(a) * sin(B) * sin(A))
+        c = arcos((cos(a) * cos(b)) + (sin(a) * sin(b) * mricxveli / mnishvneli))
+
+        mricxveli_1 = (cos(a) * cos(b_1) * sin(A) * sin(B)) - (cos(A) * cos(B))
+        mnishvneli_1 = 1 - (sin(b_1) * sin(a) * sin(B) * sin(A))
+        c_1 = arcos(
+            (cos(a) * cos(b_1)) + (sin(a) * sin(b_1) * mricxveli_1 / mnishvneli_1)
+        )
+
+        i["c"] = degrees_to_dms(c)
+        i["c_1"] = degrees_to_dms(c_1)
+
+    elif know(A, B, C):
+        mricxveli = cos(C) + (cos(A) * cos(B))
+        mnishvneli = sin(A) * sin(B)
+        c = arcos(mricxveli / mnishvneli)
+        i["c"] = degrees_to_dms(c)
+
+
+def find_a(i, c, A, B):
+    if know(A, B, c):
+        C = arcos((sin(A) * sin(B) * cos(c)) - cos(A) * cos(B))
+        a = arcos((cos(A) + (cos(B) * cos(C))) / (sin(B) * sin(C)))
+        i["a"] = degrees_to_dms(a)
+
+
+def find_b(i, a, A, B):
+    if know(A, B, a):
+        b, b_1 = arcsin(sin(a) * sin(B) / sin(A))
+        i["b"] = degrees_to_dms(b)
+        i["b_1"] = degrees_to_dms(b_1)
+
+
+def find_B(i, a, b, c, A, B, C):
+    if know(a, b, C):
+        mricxveli = sin(C) * sin(b)
+        c = arcos((cos(a) * cos(b)) + (sin(a) * sin(b) * cos(C)))
+        mnishvneli = sin(c)
+        B, B_1 = arcsin(mricxveli / mnishvneli)
+
+        if b > a and b > c:
+            if B > B_1:
+                i["B"] = degrees_to_dms(B)
+            else:
+                i["B"] = degrees_to_dms(B_1)
+        else:
+            i["B"] = degrees_to_dms(B)
+            i["B_1"] = degrees_to_dms(B_1)
+
+    elif know(a, b, A):
+        mricxveli = sin(A) * sin(b)
+        mnishvneli = sin(a)
+        B, B_1 = arcsin(mricxveli / mnishvneli)
+        if (sin(a) / sin(A)) == (sin(b) / sin(B)) == (sin(b) / sin(B_1)):
+            i["B"] = degrees_to_dms(B)
+            i["B_1"] = degrees_to_dms(B_1)
+        else:
+            if (sin(a) / sin(A)) == (sin(b) / sin(B)):
+                i["B"] = degrees_to_dms(B)
+            elif (sin(a) / sin(A)) == (sin(b) / sin(B_1)):
+                i["B_1"] = degrees_to_dms(B_1)
+            else:
+                i["B"] = None
+                i["B_1"] = None
+
+
 with open("spher.json") as f:
     data = json.load(f)
 
@@ -40,7 +181,7 @@ for i in data:
     B = i.get("B")
     A = i.get("A")
 
-    missing = {
+    """missing = {
         "a": "a" in i and i["a"] is None,
         "b": "b" in i and i["b"] is None,
         "c": "c" in i and i["c"] is None,
@@ -49,131 +190,35 @@ for i in data:
         "C": "C" in i and i["C"] is None,
     }
 
-    # find C
-    if missing["C"]:
-        if None not in (a, b, c):
-            mricxveli = cos(c) - cos(a) * cos(b)
-            mnishvneli = sin(a) * sin(b)
-            C = arcos(mricxveli / mnishvneli)
-            i["C"] = C
+    match missing:
+        case {"C": True}:
+            find_C(i, a, b, c, A, B, C)
+        case {"c": True}:
+            find_c(i, a, b, c, A, B, C)
+        case {"a": True}:
+            find_a(i, c, A, B)
+        case {"b": True}:
+            find_b(i, a, A, B)
+        case {"B": True}:
+            find_B(i, a, b, c, A, B, C)"""  # სხვა ვარიანტი რორამე
 
-        elif None not in (a, b, A):
-            B, B_1 = arcsin(sin(b) * sin(A) / sin(a))
-            mricxveli = (sin(A) * sin(B) * cos(a) * cos(b)) - cos(A) * cos(B)
-            mnishvneli = 1 - (sin(A) * sin(B) * sin(a) * sin(b))
-            C = arcos(mricxveli / mnishvneli)
-            mricxveli_1 = (sin(A) * sin(B_1) * cos(a) * cos(b)) - cos(A) * cos(B_1)
-            mnishvneli_1 = 1 - (sin(A) * sin(B_1) * sin(a) * sin(b))
-            C_1 = arcos(mricxveli_1 / mnishvneli_1)
-            i["C"] = C
-            i["C_1"] = C_1
+    unknown = next(
+        key for key in ("a", "b", "c", "A", "B", "C") if key in i and i[key] is None
+    )
 
-        elif None not in (A, B, c):
-            C = arcos((sin(A) * sin(B) * cos(c)) - cos(A) * cos(B))
-            i["C"] = C
-        elif None not in (A, B, a):
-            b, b_1 = arcsin(sin(a) * sin(B) / sin(A))
-            pirveli = (cos(a) * cos(b)) - (sin(a) * sin(b) * cos(A) * cos(B))
-            meore = 1 - (sin(b) * sin(a) * sin(B) * sin(A))
-            mricxveli = (pirveli / meore) - (cos(a) * cos(b))
-            mnishvneli = sin(a) * sin(b)
-            C = arcos(mricxveli / mnishvneli)
+    print(repr(unknown))
 
-            pirveli_1 = (cos(a) * cos(b_1)) - (sin(a) * sin(b_1) * cos(A) * cos(B))
-            meore_1 = 1 - (sin(b_1) * sin(a) * sin(B) * sin(A))
-            mricxveli_1 = (pirveli_1 / meore_1) - (cos(a) * cos(b_1))
-            mnishvneli_1 = sin(a) * sin(b_1)
-            C_1 = arcos(mricxveli_1 / mnishvneli_1)
+    match unknown:
+        case "C":
+            find_C(i, a, b, c, A, B, C)
+        case "c":
+            find_c(i, a, b, c, A, B, C)
+        case "a":
+            find_a(i, c, A, B)
+        case "b":
+            find_b(i, a, A, B)
+        case "B":
+            find_B(i, a, b, c, A, B, C)
 
-            i["C"] = C
-            i["C_1"] = C_1
-
-    # find c
-
-    elif missing["c"]:
-        if None not in (C, b, a):
-            c = arcos(cos(a) * cos(b) + sin(a) * sin(b) * cos(C))
-            i["c"] = c
-        elif None not in (a, b, A):
-            B, B_1 = arcsin(sin(b) * sin(A) / sin(a))
-            mricxveli = (cos(a) * cos(b)) - (sin(a) * sin(b) * cos(A) * cos(B))
-            mnishvneli = 1 - (sin(A) * sin(B) * sin(a) * sin(b))
-            c = arcos(mricxveli / mnishvneli)
-            mricxveli = (cos(a) * cos(b)) - (sin(a) * sin(b) * cos(A) * cos(B_1))
-            mnishvneli = 1 - (sin(A) * sin(B_1) * sin(a) * sin(b))
-            c_1 = arcos(mricxveli / mnishvneli)
-            i["c"] = c
-            i["c_1"] = c_1
-        elif None not in (a, B, A):
-            b, b_1 = arcsin(sin(a) * sin(B) / sin(A))
-
-            mricxveli = (cos(a) * cos(b) * sin(A) * sin(B)) - (cos(A) * cos(B))
-            mnishvneli = 1 - (sin(b) * sin(a) * sin(B) * sin(A))
-            c = arcos((cos(a) * cos(b)) + (sin(a) * sin(b) * mricxveli / mnishvneli))
-
-            mricxveli_1 = (cos(a) * cos(b_1) * sin(A) * sin(B)) - (cos(A) * cos(B))
-            mnishvneli_1 = 1 - (sin(b_1) * sin(a) * sin(B) * sin(A))
-            c_1 = arcos(
-                (cos(a) * cos(b_1)) + (sin(a) * sin(b_1) * mricxveli_1 / mnishvneli_1)
-            )
-
-            i["c"] = c
-            i["c_1"] = c_1
-
-        elif None not in (A, B, C):
-            mricxveli = cos(C) + (cos(A) * cos(B))
-            mnishvneli = sin(A) * sin(B)
-            c = arcos(mricxveli / mnishvneli)
-            i["c"] = c
-
-    # find a
-
-    elif missing["a"]:
-        C = arcos((sin(A) * sin(B) * cos(c)) - cos(A) * cos(B))
-        a = arcos((cos(A) + (cos(B) * cos(C))) / (sin(B) * sin(C)))
-        i["a"] = a
-
-    # find B
-
-    elif missing["B"]:
-        if None not in (a, b, C):
-            mricxveli = sin(C) * sin(b)
-            c = arcos((cos(a) * cos(b)) + (sin(a) * sin(b) * cos(C)))
-            mnishvneli = sin(c)
-            B, B_1 = arcsin(mricxveli / mnishvneli)
-
-            if b > a and b > c:
-                if B > B_1:
-                    i["B"] = B
-                else:
-                    i["B"] = B_1
-            else:
-                i["B"] = B
-                i["B_1"] = B_1
-
-        elif None not in (a, b, A):
-            mricxveli = sin(A) * sin(b)
-            mnishvneli = sin(a)
-            B, B_1 = arcsin(mricxveli / mnishvneli)
-            if (sin(a) / sin(A)) == (sin(b) / sin(B)) == (sin(b) / sin(B_1)):
-                i["B"] = B
-                i["B_1"] = B_1
-            else:
-                if (sin(a) / sin(A)) == (sin(b) / sin(B)):
-                    i["B"] = B
-                elif (sin(a) / sin(A)) == (sin(b) / sin(B_1)):
-                    i["B_1"] = B_1
-                else:
-                    i["B"] = None
-                    i["B_1"] = None
-
-    # find b
-
-    elif missing["b"]:
-        b, b_1 = arcsin(sin(a) * sin(B) / sin(A))
-        i["b"] = b
-        i["b_1"] = b_1
-
-
-with open("spher_tr.json", "w") as f:
-    json.dump(data, f, indent=4)
+with open("spher_tr.json", "w", encoding="utf-8") as f:
+    json.dump(data, f, indent=4, ensure_ascii=False)
